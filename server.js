70c5131d54d517662c673abaf7896efd66b01599
Daniel Zaoui <daniel.zaoui@yahoo.com>
2018-12-23 12:15:51 +0200

    Server: download the song and update the new items DB

--- server.js
+++ server.js
@@ -12,11 +12,14 @@ var readline = require('readline');
 var fs = require('fs');
 var path = require('path');
 var os = require('os');
+var child_process = require('child_process');
 
 // list of currently connected clients (users)
 var clients = [ ];
 // list of the current playlist items
 var cur_pl_items = [];
+// JSON list of the youtube items infos
+var yt_items = {};
 // Config data
 var config = JSON.parse(fs.readFileSync(path.join(os.homedir(), '/.config/muZic/config.json'), 'utf8'));
 
@@ -117,6 +120,52 @@ wsServer.on('request', function(request) {
                }).on("error", (err) => {
                   console.log("Error: " + err.message);
                });
+         }
+         else if (json.type == 'Select-Item')
+         {
+            console.log('Select item: ' + json.data);
+            for (let item of cur_pl_items)
+            {
+               if (item.id == json.data)
+               {
+                  var found = false;
+                  for (let yt_item in yt_items)
+                  {
+                     if (yt_item == item.id) found = true;
+                  }
+                  if (!found)
+                  {
+                     var path = '/tmp/yt-'+item.id+'.opus';
+                     const child = child_process.spawn('youtube-dl',
+                        ['--audio-format','opus', '--no-part', '-x', 'http://youtube.com/watch?v='+item.id,
+                           '-o', path]);
+                     yt_items[item.id] = ({path: path, downloading: true, is_playable: false});
+                     child.stdout.on('data', function(data)
+                        {
+                           console.log("ZZZ11"+data+"ZZZ12");
+                           if (!yt_items[item.id].is_playable)
+                           {
+                              var progress = data.toString().match(/download. ([^%]+)% of/);
+                              if (progress)
+                              {
+                                 if (progress[1] > 10.0)
+                                    yt_items[item.id].is_playable = true;
+                              }
+                           }
+                        })
+                     child.stderr.on('data', function(data)
+                        {
+                           console.log("ZZZ21"+data+"ZZZ22");
+                        })
+                     child.on('close', function()
+                        {
+                           yt_items[item.id].downloading = false;
+                           console.log(yt_items[item.id]);
+                        })
+                  }
+                  break;
+               }
+            }
          }
       }
    });
